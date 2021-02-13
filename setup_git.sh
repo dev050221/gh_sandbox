b2ca68cac93693d8bc72dae7290b5b29c69b7655
dev050221 <dev050221@github.com>
2021-02-13 20:04:17 +0530

    done

--- setup_git.sh
+++ setup_git.sh
@@ -2,6 +2,7 @@ git config --global user.name dev050221
 git config --global user.email dev050221@github.com
 git config --global core.editor vim
 git config --global merge.tool vimdiff
+git config --global pull.rebase false
 git config --global color.ui true
 git config --global color.status auto
 git config --global color.branch auto
